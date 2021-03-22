432881fa1cd3f38690653fdcf758fedbd2753416
zhanghailang123 <1187000351@qq.com>
2021-03-22 15:58:41 +0800

    聊天系统初次提交

---
+++ src/main/java/com/zhl/myimr/MyimrApplication.java
@@ -0,0 +1,13 @@
+package com.zhl.myimr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyimrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyimrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhl/myimr/MyimrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhl.myimr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyimrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
